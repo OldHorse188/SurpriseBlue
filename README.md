1df37929fbe3c77835fa18584b841280a6579cb8
OldHorse188 <MaJiaHao188@foxmail.com>
2020-07-05 16:49:29 +0800

    Modify README.md

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 程序首先判断操作系统版本，如果系统版本为 Windows 8、Windows 8.1、Windows 10则全屏展示新的死机界面，否则展示旧死机界面。  
 通过线程控制，随机增加进度，当进度达到100后，重启Windows。  
 为了恶搞的目的，添加了键盘事件，即任何触碰键盘将直接触发重启Windows。  
-![旧界面](/imgs/old.png)  
-![新界面](/imgs/new.png)  
+![旧界面](imgs/old.png)  
+![新界面](imgs/new.png)  
--- src/Start.java
+++ src/Start.java
@@ -4,25 +4,24 @@ public class Start {
     public static void main(String[] args) {
         BlueFrame bf = new BlueFrame();
 
+        JP7 jp7 = new JP7();
+        JP10 jp10 = new JP10();
 
         Properties props = System.getProperties();
         String isWin = props.getProperty("os.name");
         Double osVer = Double.parseDouble(props.getProperty("os.version"));
-        //System.out.println(props.getProperty("os.name") + "\n" + props.getProperty("os.arch") + "\n" + props.getProperty("os.version"));
-
-        // 判断系统版本，选择对应界面
         if(isWin.toLowerCase().startsWith("win") &&  osVer > 7)
         {
-            JP10 jp10 = new JP10();
             bf.add(jp10);
             new Thread(jp10).start();
         }
         else
         {
-            JP7 jp7 = new JP7();
             bf.add(jp7);
             new Thread(jp7).start();
         }
+        //System.out.println(props.getProperty("os.name") + "\n" + props.getProperty("os.arch") + "\n" + props.getProperty("os.version"));
+
         bf.open();
     }
 }
